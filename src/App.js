2a54f943a6e2d75a5d5d8d610fa75fb46812a28e
Annalise-M <nali247@gmail.com>
2021-07-20 18:36:17 -0700

    uColor created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import * as THREE from 'three';
 import { Canvas, extend } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
 import glsl from 'babel-plugin-glsl/macro';
 import './App.css';
 
 const WaveShaderMaterial = shaderMaterial(
-  // Uniform
-  {},
+  // Uniform {RGB = default of black}
+  { uColor: new THREE.Color(0.0, 0.0, 0.0) },
+
   // Vertex Shader
   glsl`
     void main() {
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
   `,
+
   // Fragment Shader
   glsl`
+    uniform vec3 uColor;
     void main() {
-      gl_FragColor = vec4(0.8, 1.0, 1.0, 1.0);
+      gl_FragColor = vec4(uColor, 1.0);
     }
   `
 );
@@ -30,7 +34,7 @@ const Scene = () => {
       <pointLight position={[10, 10, 10]} />
       <mesh>
         <planeBufferGeometry args={[3, 5]} />
-        <waveShaderMaterial />
+        <waveShaderMaterial uColor={'hotpink'}/>
       </mesh>
     </Canvas>
   );
